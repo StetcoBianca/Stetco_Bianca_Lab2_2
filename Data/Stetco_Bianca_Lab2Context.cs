08f174d40f01b838c7f010742f3b9e135060ea4a
StetcoBianca <bstetco@yahoo.ro>
2024-10-25 18:50:27 +0300

    Add project files.

---
+++ Data/Stetco_Bianca_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stetco_Bianca_Lab2.Models;
+
+namespace Stetco_Bianca_Lab2.Data
+{
+    public class Stetco_Bianca_Lab2Context : DbContext
+    {
+        public Stetco_Bianca_Lab2Context (DbContextOptions<Stetco_Bianca_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stetco_Bianca_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Stetco_Bianca_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stetco_Bianca_Lab2.Data;
+using Stetco_Bianca_Lab2.Models;
+
+namespace Stetco_Bianca_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Stetco_Bianca_Lab2.Data.Stetco_Bianca_Lab2Context _context;
+
+        public IndexModel(Stetco_Bianca_Lab2.Data.Stetco_Bianca_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
