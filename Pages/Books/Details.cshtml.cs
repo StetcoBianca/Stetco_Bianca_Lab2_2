880f49aabdfb8cd71acd454d4f3725adb5ce9bfa
StetcoBianca <bstetco@yahoo.ro>
2024-10-26 18:34:16 +0300

    Modificarea finala

--- Pages/Books/Details.cshtml.cs
+++ Pages/Books/Details.cshtml.cs
@@ -28,7 +28,12 @@ namespace Stetco_Bianca_Lab2.Pages.Books
                 return NotFound();
             }
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            var book = await _context.Book
+                .Include(b => b.Publisher)
+                .Include(b => b.BookCategories).ThenInclude(bc => bc.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+
             if (book == null)
             {
                 return NotFound();
